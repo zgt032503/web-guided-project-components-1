cf92447972de98de30d4599b3c013b7f65357db3
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-10 11:29:45 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -98,7 +98,12 @@ function makePanel({ title, content }/* what data does the panel need? */) {
       <div></div>           // panelContent
     </div>
   */
-  panel.appendChild(panelBar)
+ panel.appendChild(panelBar)
+ panel.appendChild(panelContent)
+ panelBar.appendChild(panelTitle)
+ panelBar.appendChild(panelButtons)
+ panelButtons.appendChild(openButton)
+ panelButtons.appendChild(closeButton)
 
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
