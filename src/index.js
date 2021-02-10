ff60858f1fc45b7680a72a48a0eb99c4845f85a6
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-10 11:46:00 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -123,7 +123,10 @@ function makePanel({ title, content }/* what data does the panel need? */) {
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
+  panelTitle.textContent = title
+  panelContent.textContent = content
+  openButton.textContent = open
+  closeButton.textContent = close
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
