31e9b8a645100bac43db4a5096c0d9cac215f679
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-10 11:14:25 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -66,7 +66,8 @@ console.log() // log the close arrow
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
+const accordion = document.querySelector(".accordion");
+console.log(accordion)
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
